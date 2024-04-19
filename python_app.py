c22c111efb5e73265b3e486e12a1c8b7fe4a9ae6
Devaughn Thompson <42242487+ShulkerTheDev@users.noreply.github.com>
2024-04-19 09:48:19 -0700

    Update python_app.py
    Removed brackets

--- python_app.py
+++ python_app.py
@@ -27,33 +27,29 @@ while True:
   choice = input("input your choice: ")
 
   if (choice not in choice_list)
-  {
     choice = input("Invalid choice, please enter A, B, C, D, or E:")
-  }
   else
-{
-  if choice == "a" or choice == "A":
-    print("Addition")
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
-    add(a, b)
-  elif choice == "b" or choice == "B":
-    print("Subtraction")
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
-    sub(a, b)
-  elif choice == "c" or choice == "C":
-    print("Multiplication")
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
-    mul(a, b)
-  elif choice == "d" or choice == "D":
-    print("Division")
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
-    div(a, b)
-  elif choice == "e" or choice == "E":
-    print("Program ended")
-    quit()  
-}
+    if choice == "a" or choice == "A":
+      print("Addition")
+      a = int(input("input first number: "))
+      b = int(input("input second number: "))
+      add(a, b)
+    elif choice == "b" or choice == "B":
+      print("Subtraction")
+      a = int(input("input first number: "))
+      b = int(input("input second number: "))
+      sub(a, b)
+    elif choice == "c" or choice == "C":
+      print("Multiplication")
+      a = int(input("input first number: "))
+      b = int(input("input second number: "))
+      mul(a, b)
+    elif choice == "d" or choice == "D":
+      print("Division")
+      a = int(input("input first number: "))
+      b = int(input("input second number: "))
+      div(a, b)
+    elif choice == "e" or choice == "E":
+      print("Program ended")
+      quit()  
 
